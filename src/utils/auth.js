8095501fb67a5d9ed1f35ea0800137293b1b18b2
NARUTOne <wznaruto326@163.com>
2018-03-23 17:23:02 +0800

    template update

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -7,7 +7,7 @@ const auth = {
     USER_KEY: '__REDUXUSER__',
   
     isLoginIn() {
-      const user = JSON.parse(localStorage.getItem(auth.USER_KEY))
+      const user = JSON.parse(localStorage.getItem(auth.USER_KEY));
       // 用户处于登录状态的条件：本地存储以及 cookie 同时存在
       if (user) {
         auth.user = user;
