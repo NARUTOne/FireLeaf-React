01052d76bad745c94d8df38899ca36949794ca9e
NARUTOne <wznaruto326@163.com>
2018-09-14 19:50:30 +0800

    scroll

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,7 @@
 import React, {Component} from 'react';
-
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
-
-
+import ScrollTop from 'components/ScrollTop/';
 import routers from './router.config';
-
 
 const routerList = routers.map((item, index) => {
   // console.log(item.component);
@@ -16,12 +13,14 @@ class RouterList extends Component{
   render () {
     return (
       <BrowserRouter basename="/FireLeaf">
-        <div className="wrapper">
-          <Switch>
-            {routerList}
-            {/* <Route path="*" render={({ staticContext, ...props }) => <NotFound {...props} />} /> */}
-          </Switch>
-        </div>
+        <ScrollTop>
+          <div className="wrapper">
+            <Switch>
+              {routerList}
+              {/* <Route path="*" render={({ staticContext, ...props }) => <NotFound {...props} />} /> */}
+            </Switch>
+          </div>
+        </ScrollTop>
       </BrowserRouter>
     );
   }
